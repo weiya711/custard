4043f5df78b7ced8776244ca4d9400a8076eb35d
Olivia Hsu <owhsu@stanford.edu>
2022-05-16 15:34:50 -0700

    Fix modeFormats for fiberlookups and fiberwrites

--- src/sam/sam_nodes.cpp
+++ src/sam/sam_nodes.cpp
@@ -1,3 +1,6 @@
+#include <vector>
+#include <algorithm>
+
 #include "sam_nodes.h"
 
 using namespace std;
@@ -5,10 +8,39 @@ using namespace std;
 namespace taco {
 namespace sam {
     // FiberLookupNode
+    FiberLookupNode::FiberLookupNode(const SamIR& out_ref, const SamIR& out_crd, const IndexVar& i,
+                                     const TensorVar& tensorVar, int mode, bool root, bool source, bool printEdgeName,
+                                     int nodeID)
+            : SAMNode(), out_ref(out_ref), out_crd(out_crd), tensorVar(tensorVar), mode(mode), i(i), root(root),
+            source(source), printEdgeName(printEdgeName), nodeID(nodeID) {
+        taco_iassert(mode < tensorVar.getOrder());
+        if (mode >= 0) {
+            auto modeOrders = tensorVar.getFormat().getModeOrdering();
+            auto it = std::find(modeOrders.begin(), modeOrders.end(), mode);
+            auto loc = std::distance(modeOrders.begin(), it);
+
+            modeFormat = tensorVar.getFormat().getModeFormats().at(loc);
+        }
+    }
+
     std::string FiberLookupNode::getName() const {
         stringstream ss;
         ss << "FiberLookup " << i.getName() << ": " << tensorVar.getName() << to_string(mode) << "\\n" << modeFormat.getName();
         return ss.str();
+    }
+
+    FiberWriteNode::FiberWriteNode(IndexVar& i, const TensorVar& tensorVar, int mode, std::string maxSegSize,
+                                   std::string maxCrdSize, bool sink, bool vals, int nodeID)
+    : SAMNode(), tensorVar(tensorVar), mode(mode), i(i), maxSegSize(maxSegSize), maxCrdSize(maxCrdSize),
+    sink(sink), vals(vals), nodeID(nodeID) {
+        taco_iassert(mode < tensorVar.getOrder());
+        if (mode >= 0) {
+            auto modeOrders = tensorVar.getFormat().getModeOrdering();
+            auto it = std::find(modeOrders.begin(), modeOrders.end(), mode);
+            auto loc = std::distance(modeOrders.begin(), it);
+
+            modeFormat = tensorVar.getFormat().getModeFormats().at(loc);
+        }
     }
 
     std::string FiberWriteNode::getName() const {
